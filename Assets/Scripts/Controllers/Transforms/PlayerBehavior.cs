f7d8ca84856c11a6767e337c401a06ea49a6a56f
Lawrence Horton <poxopox1995@gmail.com>
2016-07-17 17:02:18 -0400

    added lookscripts and move scripts with a utility file

---
+++ Assets/Scripts/Controllers/Transforms/PlayerBehavior.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+    public GameObject self;
+    public GameObject head;
+    private TransformGameObject MoveGO;
+    private float mousex, mousey;
+    public float mousexSensitivity, mouseySensitivity;
+    // Use this for initialization
+    void Start () {
+        MoveGO = self.AddComponent<TransformGameObject>();
+        mousex = Input.GetAxisRaw("Mouse X");
+        mousey = Input.GetAxisRaw("Mouse Y");
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void MovePlayer() {
+        float movez = Input.GetAxis("Vertical");
+        float movex = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(-movex, 0.0F, -movez);
+        MoveGO.TransformGO(self, movement);
+    }
+    void PlayerLook() {
+        Vector3 playerLook = new Vector2(mousex * mousexSensitivity, mousey * mouseySensitivity);
+        MoveGO.LookGO(head, self, playerLook);
+    }
+	// Update is called once per frame
+	void Update () {
+        MovePlayer();
+        PlayerLook();
+        if (Input.GetKey(KeyCode.Escape)) {
+            Cursor.lockState = CursorLockMode.None;
+        }
+	}
+    void LateUpdate() {
+        mousex = Input.GetAxisRaw("Mouse X");
+        mousey = Input.GetAxisRaw("Mouse Y");
+    }
+
+
+}
---
+++ Assets/Scripts/Utilities/TransformGameObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformGameObject : MonoBehaviour {
+
+    public void TransformGO (GameObject go, Vector3 movement) {
+        go.transform.Translate(movement);
+    }
+    public void LookGO(GameObject head, GameObject body, Vector2 mousePosition) {
+
+        body.transform.Rotate(Vector3.up * mousePosition.x);
+        head.transform.Rotate(Vector3.right * mousePosition.y);
+            
+        
+
+    }
+}

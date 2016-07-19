5e3674543c0a14e0febc63dea72f564784ecd086
Lawrence Horton <poxopox1995@gmail.com>
2016-07-19 00:28:48 -0400

    added aiming system

--- Assets/Scripts/Controllers/Mechanics/WeaponBehavior.cs
+++ Assets/Scripts/Controllers/Mechanics/WeaponBehavior.cs
@@ -16,30 +16,61 @@ public class WeaponBehavior : MonoBehaviour {
 
     void LaunchProjectile()
     {
-        if (animation.GetCurrentAnimatorStateInfo(0).IsName("Fire")) {}else
+        if (animation.GetCurrentAnimatorStateInfo(0).IsName("Fire") || animation.GetCurrentAnimatorStateInfo(0).IsName("AimFire")) { } else
         {
+            
             var init = projectile;
             var position = weapon.transform.position + (weapon.transform.forward * offsetx) + (weapon.transform.right * offsety) + (weapon.transform.up * offsetz);
-
             var clone = Instantiate(init, position, weapon.transform.rotation) as Rigidbody;
             clone.AddForce(clone.transform.right * force);
-            animation.Play("Fire");
+            if (animation.GetCurrentAnimatorStateInfo(0).IsName("Aim"))
+            {
+                animation.Play("AimFire");
+            }
+            else {
+                animation.Play("Fire");
+            }
         }
-        
+
     }
 
+    void Aim() {
+        if (animation.GetCurrentAnimatorStateInfo(0).IsName("Aim")){}
+        else {animation.Play("Aim");}
+    }
+    void UnAim() {
+        if (animation.GetCurrentAnimatorStateInfo(0).IsName("idle")) { }
+        else { animation.Play("idle"); }
+    }
 
+    void AimFire() {
+        LaunchProjectile();
+        if (animation.GetCurrentAnimatorStateInfo(0).IsName("AimFire")) { }
+        else { animation.Play("AimFire"); }
 
-
+    }
     public void Fire() {
-       // PlayAnimation();
         LaunchProjectile();
     }
 
     void LateUpdate() {
-        if (Input.GetButtonDown("Fire1"))
+
+        if (Input.GetButtonDown("Fire1")) {
+            if (Input.GetButtonDown("Fire2")) {
+                AimFire();
+            }
+            else {
+                Fire();
+            };
+
+        }
+        if (Input.GetButtonDown("Fire2"))
         {
-            Fire();
+            Aim();
+        }
+        if (Input.GetButtonUp("Fire2"))
+        {
+            UnAim();
         }
     }
 }
